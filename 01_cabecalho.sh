9386efb862311cb3630e06dbdc7591dd9ec7d013
Bernardo <mrochabernardo@gmail.com>
2022-07-31 12:58:36 -0300

    Começo do livro

---
+++ 01_cabecalho.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 01_cabecalho.sh
+#
+# Um bom profissional sabe como fazer um cabeçalho de qualidade
+# Script com propósito de servir de molde para cabeçalhos
+# Autor       :Bernardo Monteiro Rocha - GitHub && Codeberg @bernaRocha
+# Manutenção  :Bernardo Monteiro Rocha 
+# Criação em 31/07/2022
+#
+# Licença GPL
+
+ 
+# Comentários especiais {
+
+    #TODO  > Indica uma tarefa a ser feita
+
+    #FIXME > Algo a ser corrigido
+
+    #XXX   > Algo importante que precisa de atenção
+
+# }
